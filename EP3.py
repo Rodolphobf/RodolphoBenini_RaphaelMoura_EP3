005263340876357f9bdeada0c01a52ee9a123826
Rodolphobf <rodolphobenini@yahoo.com.br>
2015-04-16 15:10:49 -0300

    Versao 3

--- EP3.py
+++ EP3.py
@@ -1,46 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-from Funcoes_EP3 import *
 
-
-alimento = open("alimentos.csv")
+alimento = open("alimentos.csv")   #abrir arquivos
 usuario = open("usuario (1).csv")
+usuario1 = usuario.readlines() #ler usuarios
 
-alimentos = []   #lista quebrada
-alimento.r   eadlines() #ler alimentos
-alimentos = alimento.split(",")
+alimentos = alimento.readlines() #ler alimentos
+   
+   
+l = {}
 
-usuarioslista = []  #lista quebrada
-usuario.readlines() #ler usuarios
-usuarioslista = usuario.split(",") 
-
-nome = input("Qual o seu nome?/n")
-sexo = input("Qual seu sexo/n")
-idade = input("Qual a sua idade/n")
-peso = input("Qual o seu peso")
-altura = input("Qual a sua altura?")
-AF = input("Classifique sua atividade fisica semanal de 1 a 5")
-
-if sexo == "homem" or 'Homem':
-    tmb = massa_homem(peso,idade,altura)
-    
-if sexo == "mulher" or "Mulher":
-    tmb = massa_mulher(peso,idade,altura)
-gviuv
-if AF == 1:
-    caloria1=tmb*1.2
-    
-if AF == 2:
-    caloria1=tmb*1.375
-
-if AF == 3:
-    caloria1=tmb*1.55viv
-
-if AF == 4:
-    caloria1=tmb*1.725
-
-if AF == 5:
-    caloria1=tmb*1.9
-    
+for i in range(1,len(alimentos)):
+    alimentos[i]=alimentos[i].strip()
+    a = alimentos[i].split(',')
+    l[a[0]] = a[1:4]
+    print(l)      
     
