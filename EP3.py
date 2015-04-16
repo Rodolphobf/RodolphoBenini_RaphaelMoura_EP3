d62dd08559329006b00bc2d7a5fa3d880d6ad36c
Rodolphobf <rodolphobenini@yahoo.com.br>
2015-04-16 15:28:20 -0300

    Versao 4

--- EP3.py
+++ EP3.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 
 alimento = open("alimentos.csv")   #abrir arquivos
 usuario = open("usuario (1).csv")
-usuario1 = usuario.readlines() #ler usuarios
 
 alimentos = alimento.readlines() #ler alimentos
    
@@ -15,5 +14,19 @@ for i in range(1,len(alimentos)):
     alimentos[i]=alimentos[i].strip()
     a = alimentos[i].split(',')
     l[a[0]] = a[1:4]
-    print(l)      
+  
+usuario1 = usuario.readlines() #ler usuarios
+
+u = {}
+
+for i in range(3,len(usuario1)):
+    usuario1[i]=usuario1[i].strip()
+    b = usuario1[i].split(',')
+    u[b[0]] = b[1:len(b)]        
+    print(u)
     
+linha_user = usuario1[1]
+
+lista_user = linha_user.split(",")
+
+print(lista_user)
