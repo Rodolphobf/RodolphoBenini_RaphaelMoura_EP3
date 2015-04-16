adf49f02e5707a51f64d07d248dab9eb42043a74
Rodolphobf <rodolphobenini@yahoo.com.br>
2015-04-16 08:56:52 -0300

    Versao 1

---
+++ EP3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+alimentos = open("alimentos.csv")
+usuario = open("usuario (1).csv")
+
+alimentos.readlines() #ler alimentos
+alimentos.split(",")
+
+usuario.readlines() #ler usuarios
+usuario.split(",") 
+
+nome = input("Qual o seu nome?/n")
+sexo = input("Qual seu sexo/n")
+idade = input("Qual a sua idade/n")
+peso = input("Qual o seu peso")
+altura = input("Qual a sua altura?")
+AF = input("Classifique sua atividade fisica semanal de 1 a 5")
+
+if sexo == homem or Homem:
+    massa_homem()
+    
---
+++ Funcoes_EP3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 08:53:40 2015
+
+@author: rodolphobeninifilgueiras
+"""
+
