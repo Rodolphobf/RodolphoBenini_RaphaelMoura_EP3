3fdf3b6962e06705e439e123fb874b5222b45e1d
Rodolphobf <rodolphobenini@yahoo.com.br>
2015-04-22 19:31:36 -0300

    Versao 6

--- EP3.py
+++ EP3.py
@@ -32,40 +32,32 @@ linha_user = usuario1[1]
 lista_user = linha_user.split(",")
 
 nome = lista_user[0]
-idade = int(lista_user[1])
+idade = float(lista_user[1])
 peso = float(lista_user[2])
 sexo = str(lista_user[3])
-sexo = sexo.lower()
+sexo = sexo.upper()
 altura = float(lista_user[4])
-FF = lista_user[5]
-print(lista_user[3])
+FF = str(lista_user[5])
+FF = FF.lower()
+print(lista_user[5])
+
 
 if sexo == "M":
-    massa_homem(peso,idade,altura)   
-    if FF == "minimo":
-        TMB = massa_homem*1.2   
-    if FF == 'baixo':
-        TMB = massa_homem*1.375
-    if FF == 'medio' :
-        TMB = massa_homem*1.55
-    if FF == 'alto':
-        TMB = massa_homem*1.725
-    if FF == 'muito alto':
-        TMB = massa_homem*1.9
-
-
+    CPV = massa_homem(peso,idade,altura)
+    
 if sexo == "F":
-    massa_mulher(peso,idade,altura)
-    if FF == "minimo":
-        TMB = massa_mulher*1.2
-    if FF == 'baixo':
-        TMB = massa_mulher*1.375
-    if FF == 'medio' :
-        TMB = massa_mulher*1.55
-    if FF == 'alto':
-        TMB = massa_mulher*1.725
-    if FF == 'muito alto':
-        TMB = massa_mulher*1.9
-        
-print(TMB)
+    CPV = massa_mulher(peso,idade,altura)
 
+    
+if FF == "minimo":
+    CPV = CPV*1.2
+if FF == 'baixo':
+    CPV = CPV*1.375
+if FF == 'medio' :
+    CPV = CPV*1.55
+if FF == 'alto':
+    CPV = CPV*1.725
+if FF == 'muito alto':
+    CPV = CPV*1.9    
+
+print(CPV)
